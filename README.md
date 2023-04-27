5719cdf0cb846a9d7db6fa4d860d3d83f1f27bf9
Andriță Lucian-Gabriel <gabylucian2000@gmail.com>
2023-04-27 15:56:29 +0300

    Added script for first time setup

--- README.md
+++ README.md
@@ -1,39 +1,48 @@
-    Boost v1.67.0
-we use only header files for now
-    
-    Poco v1.12.4
-https://github.com/pocoproject/poco/tree/master
-sudo apt-get install openssl libssl-dev
-sudo apt-get -y update && sudo apt-get -y install git g++ make cmake libssl-dev
-cd libs/poco/build
-cmake ..
-cmake --build . --config Release
-sudo cmake --build . --target install
-
-    SASS v1.58.3
-vim ~/.bashrc
-export PATH="absolute_path_to_executable"
-
-    NPM v8.19.2
-vim ~/.bashrc
-export PATH=$PATH:/mnt/c/Users/INSERT-USER/AppData/Roaming/npm
-
-    GCP v2.9.1
-https://github.com/googleapis/google-cloud-cpp
-
-    GC CLI v427.0.0
-https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-427.0.0-linux-x86_64.tar.gz
-    Commands for quickstart dialogflow-es
-gcloud config set project PROJECT_ID
-gcloud config set billing/quota_project PROJECT_ID
     We create a new key for an already existing service account (the default one)
-Docs: https://cloud.google.com/iam/docs/keys-create-delete#iam-service-account-keys-create-console
+https://console.cloud.google.com/iam-admin/serviceaccounts/details/106927336924460841238/keys?walkthrough_id=iam--create-service-account-keys&project=licenta-383311&supportedpurview=project
     After we download the json we store in ./res. Name is not relevant as long as there we'll be only one json 
 file in that folder's root.
 
     VCPKG v2023.04.15 Release
 https://github.com/microsoft/vcpkg.git
 
+    Steps for enviroment setup:
+Steps:
+    git submodule update --init --recursive
+    git checkout to a specific version (for each library)
+    dos2unix for ./build.sh and ./libs/dar-sass/sass
+
+        Poco v1.12.4
+    1. sudo apt-get install openssl libssl-dev
+    2. sudo apt-get -y update && sudo apt-get -y install git g++ make cmake libssl-dev
+    3. cd ./libs/poco/build
+    4. cmake ..
+    5. cmake --build . --config Release
+    6. sudo cmake --build . --target install
+
+        Npm v8.19.2
+    1. sudo apt-get install npm
+
+        GC CLI v427.0.0
+    1. curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-427.0.0-linux-x86_64.tar.gz
+    2. tar -xf google-cloud-cli-427.0.0-linux-x86.tar.gz
+    3. ./google-cloud-sdk/install.sh
+    4. source ~/.bashrc
+    5. ./google-cloud-sdk/bin/gcloud init
+
+        SASS v1.58.3
+    1. sudo ln -s /mnt/c/Users/gabyl/OneDrive/Desktop/FACULTATE/Anul-4/BachelorsDegree/libs/dart-sass/sass /usr/local/bin/sass
+
+        VCPKG v2023.04.15 Release
+    1. ./libs/vcpkg/bootstrap-vcpkg.sh
+
+        GCP v2.9.1 (only core and dialogflow-es)
+    1. cd ./libs/vcpkg
+    2. ./vcpkg install google-cloud-cpp[core,dialogflow-es]
+
+Optional steps(requires gc cli installed(steps 0.2-0.6)):
+gcloud config set project PROJECT_ID
+gcloud config set billing/quota_project PROJECT_ID
+
     How to build:
-Open the Proiect folder as workspace, make sure you have executable 
-rights on the build.sh file and then run it using: ./build.sh -b
+Open the Proiect folder as workspace, make sure you have executable rights on the build.sh file and it's formatted for unix (dos2unix) and then run it using: ./build.sh -b
---
+++ getting_started.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+clear
+read -r -d '\0' USAGE <<- EOM
+Usage: per default the script doesn't run.
+  -i|--install       - installs and configures all the prerequisites for the project
+  -v|--verbose       - optional, print more output to the console
+  -h|--help          - show this help
+EOM
+
+VERBOSE="false"
+
+#### functions
+err_exit() { echo -e "$(tput setaf 1)$(tput bold)"ERROR: "$(tput sgr0)$* \n" >&2; exit 1; }
+wrn() { echo "$(tput setaf 3)$(tput bold)"WARNING: "$(tput sgr0)$*" >&2; }
+msg() { echo -e "$(tput bold)"INFO: "$(tput sgr0)$* \n"; }
+msg_ne() { echo -ne "$(tput bold)"INFO: "$(tput sgr0)$*"; }
+succ() { echo -e "$(tput setaf 2)$(tput bold)"SUCCESS: "$(tput sgr0)$* \n";}
+inf() { echo -e "$(tput bold)"INFO: "$(tput sgr0)$* \n"; }
+
+if [[ $EUID -ne 0 ]]; then
+   err_exit "This script must be run with sudo privileges." 1>&2
+   exit 1
+fi
+
+change_versions() {
+    msg "Getting wanted versions for submodules..."
+        # Poco v1.12.4
+    cd ./libs/poco
+    git checkout poco-1.12.4-release
+    cd ..
+        # VCPKG v2023.04.15 Release
+    cd ./libs/vcpkg
+    git checkout 2023.04.15
+    cd..
+        # GCP v2.9.1
+    cd ./libs/google-cloud-cpp
+    git checkout 2.9.1
+    cd ..
+}
+
+enviroment_setup() {
+    msg "Enviroment setup..."
+    git submodule update --init --recursive
+    change_versions
+    dos2unix ./build.sh ./libs/dart-sass/sass
+}
+
+poco_setup() {
+    msg "Poco setup..."
+    apt-get install openssl libssl-dev
+    apt-get -y update && apt-get -y install git g++ make cmake libssl-dev
+    cd ./libs/poco/build
+    cmake ..
+    cmake --build . --config Release
+    cmake --build . --target install
+    cd ../../../
+}
+
+npm_setup() {
+    msg "Npm setup..."
+    apt-get install npm
+}
+
+gc_cli_setup() {
+    msg "GC CLI setup..."
+    curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-427.0.0-linux-x86_64.tar.gz
+    tar -xf google-cloud-cli-427.0.0-linux-x86.tar.gz
+    ./google-cloud-sdk/install.sh
+    source ~/.bashrc
+    ./google-cloud-sdk/bin/gcloud init
+}
+
+sass_setup() {
+    msg "Sass setup..."
+    ln -s /mnt/c/Users/gabyl/OneDrive/Desktop/FACULTATE/Anul-4/BachelorsDegree/libs/dart-sass/sass /usr/local/bin/sass
+}
+
+vcpkg_setup() {
+    msg "VCPKG setup..."
+    ./libs/vcpkg/bootstrap-vcpkg.sh
+}
+
+gcp_setup() {
+    msg "GCP setup (this will take ~1.5h)..."
+    ./libs/vcpkg install google-cloud-cpp[core,dialogflow-es]
+}
+
+commands=("enviroment_setup" "poco_setup" "npm_setup" "gc_cli_setup" "sass_setup" "vcpkg_setup" "gcp_setup")
+
+install() {
+    for command in "${commands[@]}"; do
+        if [ "${VERBOSE}" = "false" ]; then
+            eval "$command" > /dev/null
+            exit_status=$?
+        else
+            eval "$command"
+            exit_status=$?
+        fi
+        if [[ $exit_status -ne 0 ]]; then
+            err_exit "Failed with exit status $exit_status!\n"
+        else
+            succ "Done"
+        fi
+    done
+}
+
+execute() {
+    case $COMMAND in
+        'INSTALL')
+            install
+        ;;
+        'NONE')
+            err_exit "No command was given!"
+        ;;
+    esac
+}
+
+func_read_cli_options() {
+    while [ $# -ne 0 ]; do
+        case "$1" in
+            '-i'|'--install')
+                COMMAND="INSTALL"
+        shift;;
+            '-v'|'--verbose')
+                # verbose mode
+                VERBOSE="true"
+        shift;;
+            '-h'|'--help') 
+                msg "$USAGE"; 
+                exit 0 
+        ;;
+
+        *) msg "$USAGE"; err_exit "Unknown parameter '$1'" ;;
+
+        '') break ;;
+    esac
+    done
+    execute
+}
+
+MY_EXIT_CODE=0;
+COMMAND="NONE"
+func_read_cli_options "$@";
+echo
+exit ${MY_EXIT_CODE}
