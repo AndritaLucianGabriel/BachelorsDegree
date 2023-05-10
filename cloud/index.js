6615b3224e5a98e3d4545a8151b7573307ee1996
Andrita Lucian-Gabriel <gabylucian2000@gmail.com>
2023-05-10 11:24:17 +0000

    Fixed

--- cloud/index.js
+++ cloud/index.js
@@ -29,6 +29,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
     const fileContent = 'Hello, this is a sample content!';
     const bucketName = 'licenta_data';
     agent.add(`File '${fileName}' has been written to bucket '${bucketName}'`);
+    
     const bucket = storage.bucket(createBucketIfNotExists(bucketName));
     const fileName = 'example.txt';
     const file = bucket.file(fileName);
