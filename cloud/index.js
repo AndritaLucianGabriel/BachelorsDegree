4dad42b885223804db3c09fbfe8596ab18dfb904
Andrita Lucian-Gabriel <gabylucian2000@gmail.com>
2023-05-10 11:50:28 +0000

    3

--- cloud/index.js
+++ cloud/index.js
@@ -27,6 +27,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   // Testing
   function checkBalance(agent) {
     agent.add(`Hatz`);
+    return;
     // const fileContent = 'Hello, this is a sample content!';
     // const bucketName = 'licenta_data';
     // agent.add(`File '${fileName}' has been written to bucket '${bucketName}'`);
